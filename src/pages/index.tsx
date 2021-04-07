fab99f74eebaa65b5553c4b48294fa319d723cc8
Eduardo Nogueira <eduardodudunog@gmail.com>
2021-04-07 13:12:36 -0300

    Quinto dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useState } from "react";
+import { GetServerSideProps } from "next";
 
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
@@ -8,12 +8,13 @@ import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
+import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
+import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
-
-import { CountdownProvider } from "../contexts/CountdownContext";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,7 +25,13 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   const classes = useStyles();
   const [open, setOpen] = useState(true);
 
@@ -37,33 +44,51 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
-      <ExperienceBar />
-      <br /> <br />
-      <div className={classes.root}>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} variant="filled" severity="warning">
-            Este site ainda está sendo desenvolvido!
-          </Alert>
-        </Snackbar>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+        <ExperienceBar />
+        <br /> <br />
+        <div className={classes.root}>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} variant="filled" severity="warning">
+              Este site ainda está sendo desenvolvido!
+            </Alert>
+          </Snackbar>
+        </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <br /> <br />
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <br /> <br />
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
       </div>
-      <CountdownProvider>
-        <section>
-          <div>
-            <br /> <br />
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <br /> <br />
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+    </ChallengesProvider>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  };
+};
